9c3e698c30d0d831526b78b27c366ce30e66edb5
wanzx <2388824995@qq.com>
2025-05-29 17:28:57 +0800

    feat: 1.0 模块搭建，日志切面

---
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.quanxiaoha.xiaohashu.auth.controller;
+
+import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
+import com.quanxiaoha.framework.common.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    @ApiOperationLog(description = "测试接口")
+    public Response<String> test() {
+        return Response.success("Hello, 犬小哈专栏");
+    }
+
+    @GetMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2() {
+        return Response.success(User.builder()
+                .nickName("犬小哈")
+                .createTime(LocalDateTime.now())
+                .build());
+    }
+}
+
---
+++ xiaohashu-auth/src/test/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.xiaohashu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohashuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
