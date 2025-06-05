48256d1e2d6ac3d1fd96fc01104542ff0121c950
wanzx <sx_wanzx@canway.net>
2025-06-05 16:23:01 +0800

    feat: 4.7 自定义 Jackson 配置

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/controller/TestController.java
@@ -3,6 +3,8 @@ package com.quanxiaoha.xiaohashu.auth.controller;
 import com.quanxiaoha.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.quanxiaoha.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -17,7 +19,7 @@ public class TestController {
         return Response.success("Hello, 犬小哈专栏");
     }
 
-    @GetMapping("/test2")
+    @GetMapping("/test1")
     @ApiOperationLog(description = "测试接口2")
     public Response<User> test2() {
         return Response.success(User.builder()
@@ -25,5 +27,12 @@ public class TestController {
                 .createTime(LocalDateTime.now())
                 .build());
     }
+
+    @PostMapping("/test2")
+    @ApiOperationLog(description = "测试接口2")
+    public Response<User> test2(@RequestBody User user) {
+        return Response.success(user);
+    }
+
 }
 
