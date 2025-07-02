0df5f3ba3d77a21edde1fc5ae146aae86f972c33
wanzx <sx_wanzx@canway.net>
2025-07-02 17:08:40 +0800

    feat: 9.1 用户微服务搭建(1)

---
+++ xiaohashu-user/xiaohashu-user-biz/src/main/java/com/quanxiaoha/xiaohashu/user/biz/XiaohashuUserBizApplication.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.xiaohashu.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohashuUserBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohashuUserBizApplication.class, args);
+    }
+
+}
